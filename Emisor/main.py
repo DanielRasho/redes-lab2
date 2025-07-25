617ed5775d8f73db7ff4d8f246b991de2df6bcbb
DanielRasho <ray22933@uvg.edu.gt>
2025-07-24 19:58:43 -0600

    feat: reorganize repository

---
+++ Emisor/main.py
@@ -0,0 +1,20 @@
+def add_noise(bits, noise_rate):
+    '''
+        Return a new array with the final value
+    '''
+    pass
+
+def text_to_bits(str):
+    '''
+        Return an array of bits
+    '''
+    pass
+
+def apply_algorithm(str, algorithm=""):
+    pass
+
+if __name__ == '__main__':
+    str = input("Ingresar mensaje: \n")
+    algorithm = input("Ingresar")
+    
+    # Start Socket connection
