8b18c2c03ca0c07c049452274ca139d7d98289e3
DanielRasho <ray22933@uvg.edu.gt>
2025-07-24 20:42:50 -0600

    feat: implement text_to_bits

--- Emisor/main.py
+++ Emisor/main.py
@@ -8,13 +8,21 @@ def text_to_bits(str):
     '''
         Return an array of bits
     '''
-    pass
+    final = []
+    final.append(format(len(str), '08b'))
+    for c in str:
+        final.append(format(ord(c), '08b'))
+    
+    return final
 
 def apply_algorithm(str, algorithm=""):
     pass
 
 if __name__ == '__main__':
-    str = input("Ingresar mensaje: \n")
-    algorithm = input("Ingresar")
+    # str = input("Ingresar mensaje: \n")
+    # algorithm = input("Ingresar")
     
     # Start Socket connection
+
+    a = text_to_bits("helloWorld")
+    print(a)
